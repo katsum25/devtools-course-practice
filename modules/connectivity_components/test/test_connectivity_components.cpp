34c912663fe1b119be67b8eaed79f538cee7d123
katsum25 <pestreev00@mail.ru>
2021-04-24 14:46:44 +0300

    fix2

--- modules/connectivity_components/test/test_connectivity_components.cpp
+++ modules/connectivity_components/test/test_connectivity_components.cpp
@@ -7,101 +7,115 @@
 #include "include/connectivity_components.h"
 
 TEST(Pestreev_connectivity_components, constructor) {
-    ASSERT_NO_THROW(Graph g2(10));
+    ASSERT_NO_THROW(Graph graph(10));
 }
 
 TEST(Pestreev_connectivity_components, no_vertexes) {
-    Graph g2(0);
-    int res = g2.countConectComps();
+    Graph graph(0);
+
+    int res = graph.countConectComps();
+
     ASSERT_EQ(0, res);
 }
 
 TEST(Pestreev_connectivity_components, graph_with_one_vertex) {
     int N = 1;
-    Graph g2(N);
+    Graph graph(N);
     for (int i = 0 ; i < N; i++) {
         for (int j = i; j < N; j++) {
-            g2.element(i, j) = 0;
-            g2.element(j, i) = g2.element(i, j);
-            if (i == j) g2.element(i, j) = 0;
+            graph.element(i, j) = 0;
+            graph.element(j, i) = graph.element(i, j);
+            if (i == j) graph.element(i, j) = 0;
         }
     }
-    int res = g2.countConectComps();
+
+    int res = graph.countConectComps();
+
     ASSERT_EQ(1, res);
 }
 
 TEST(Pestreev_connectivity_components, out_of_scope_index) {
     int N = 5;
-    Graph g2(N);
+    Graph graph(N);
+
     for (int i = 0 ; i < N; i++) {
         for (int j = i; j < N; j++) {
-            g2.element(i, j) = i;
-            g2.element(j, i) = g2.element(i, j);
-            if (i == j) g2.element(i, j) = 0;
+            graph.element(i, j) = i;
+            graph.element(j, i) = graph.element(i, j);
+            if (i == j) graph.element(i, j) = 0;
         }
     }
-    ASSERT_ANY_THROW(g2.element(6, 6));
+
+    ASSERT_ANY_THROW(graph.element(6, 6));
 }
 
 TEST(Pestreev_connectivity_components, out_of_scope_index_2) {
     int N = 5;
-    Graph g2(N);
+    Graph graph(N);
+
     for (int i = 0 ; i < N; i++) {
         for (int j = i; j < N; j++) {
-            g2.element(i, j) = i;
-            g2.element(j, i) = g2.element(i, j);
-            if (i == j) g2.element(i, j) = 0;
+            graph.element(i, j) = i;
+            graph.element(j, i) = graph.element(i, j);
+            if (i == j) graph.element(i, j) = 0;
         }
     }
-    ASSERT_ANY_THROW(g2.element(-1, 3));
+
+    ASSERT_ANY_THROW(graph.element(-1, 3));
 }
 
 
 TEST(Pestreev_connectivity_components, complete_simple_graph) {
     int N = 10;
-    Graph g2(N);
+    Graph graph(N);
     for (int i = 0 ; i < N; i++) {
         for (int j = i; j < N; j++) {
-            g2.element(i, j) = 1;
-            g2.element(j, i) = g2.element(i, j);
-            if (i == j) g2.element(i, j) = 0;
+            graph.element(i, j) = 1;
+            graph.element(j, i) = graph.element(i, j);
+            if (i == j) graph.element(i, j) = 0;
         }
     }
-    int res = g2.countConectComps();
+
+    int res = graph.countConectComps();
+
     ASSERT_EQ(1, res);
 }
 
 TEST(Pestreev_connectivity_components, empty_graph) {
     int N = 10;
-    Graph g2(N);
+    Graph graph(N);
     for (int i = 0 ; i < N; i++) {
         for (int j = i; j < N; j++) {
-            g2.element(i, j) = 0;
-            g2.element(j, i) = g2.element(i, j);
-            if (i == j) g2.element(i, j) = 0;
+            graph.element(i, j) = 0;
+            graph.element(j, i) = graph.element(i, j);
+            if (i == j) graph.element(i, j) = 0;
         }
     }
-    int res = g2.countConectComps();
+
+    int res = graph.countConectComps();
+
     ASSERT_EQ(10, res);
 }
 
 TEST(Pestreev_connectivity_components, multi_graph) {
     int N = 10;
-    Graph g2(N);
+    Graph graph(N);
     for (int i = 0 ; i < N; i++) {
         for (int j = i; j < N; j++) {
-            g2.element(i, j) = i;
-            g2.element(j, i) = g2.element(i, j);
-            if (i == j) g2.element(i, j) = 0;
+            graph.element(i, j) = i;
+            graph.element(j, i) = graph.element(i, j);
+            if (i == j) graph.element(i, j) = 0;
         }
     }
-    int res = g2.countConectComps();
+
+    int res = graph.countConectComps();
+
     ASSERT_EQ(2, res);
 }
 
 TEST(Pestreev_connectivity_components, some_simple_graph) {
     int N = 7;
-    Graph g2(N);
+    Graph graph(N);
     std::vector<std::vector<int>> matrix = {
         {0, 0, 1, 1, 0, 0, 0},
         {0, 0, 0, 1, 0, 0, 0},
@@ -113,16 +127,18 @@ TEST(Pestreev_connectivity_components, some_simple_graph) {
     };
     for (int i = 0 ; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            g2.element(i, j) = matrix[i][j];
+            graph.element(i, j) = matrix[i][j];
         }
     }
-    int res = g2.countConectComps();
+
+    int res = graph.countConectComps();
+
     ASSERT_EQ(3, res);
 }
 
 TEST(Pestreev_connectivity_components, some_multi_graph) {
     int N = 8;
-    Graph g2(N);
+    Graph graph(N);
     std::vector<std::vector<int>> matrix = {
         {0, 0, 0, 0, 0, 0, 0, 1},
         {0, 0, 0, 0, 0, 0, 0, 1},
@@ -135,9 +151,11 @@ TEST(Pestreev_connectivity_components, some_multi_graph) {
     };
     for (int i = 0 ; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            g2.element(i, j) = matrix[i][j];
+            graph.element(i, j) = matrix[i][j];
         }
     }
-    int res = g2.countConectComps();
+
+    int res = graph.countConectComps();
+
     ASSERT_EQ(2, res);
 }
